c569850e0d3cf5f9933d0b799d62ac5c490a5cbd
Cristian Lopez <c88lopez@gmail.com>
2021-06-02 00:36:16 -0300

    fixed tests...

--- src/functional-math/sub.test.ts
+++ src/functional-math/sub.test.ts
@@ -1,12 +1,12 @@
 const { sub } = require('./sub');
 
 test.each([
-  [3, 4, 7],
-  [1, 2, 3],
-  [2, 1, 3],
-  [1, 1, 2],
-  [0, 1, 1],
+  [3, 4, -1],
+  [1, 2, -1],
+  [2, 1, 1],
+  [1, 1, 0],
+  [0, 1, -1],
   [0, 0, 0],
-])('subtract %i - %i to equal %i', () => {
-  expect(sub(3, 1)).toEqual(2);
+])('subtract %i - %i to equal %i', (a, b, expected) => {
+  expect(sub(a, b)).toEqual(expected);
 });
--- src/functional-math/sum.test.ts
+++ src/functional-math/sum.test.ts
@@ -1,12 +1,12 @@
 const { sum } = require('./sum');
 
 test.each([
-  [3, 4, -1],
-  [1, 2, -1],
-  [2, 1, 1],
-  [1, 1, 0],
-  [0, 1, -1],
+  [3, 4, 7],
+  [1, 2, 3],
+  [2, 1, 3],
+  [1, 1, 2],
+  [0, 1, 1],
   [0, 0, 0],
-])('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toEqual(3);
+])('adds %i + %i to equal %i', (a, b, expected) => {
+  expect(sum(a, b)).toEqual(expected);
 });
